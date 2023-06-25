b86bd763d8aeb298a7145ea3d564b52db0800f36
rbalajee <rbalajee@brightcove.com>
2023-06-25 16:13:06 +0530

    added new route to support JVOD state changes

---
+++ handlers/jvodstates.go
@@ -0,0 +1,48 @@
+package handlers
+
+import (
+	"encoding/json"
+	_ "expvar"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+// Fields that will be read from the request body when unmarshalling to output
+type JVOD struct {
+	JobID     string `json:"job_id"`
+	Event     string `json:"event"`
+	JvodState string `json:"jvod_state"`
+}
+
+func JVODStateChangeListener(w http.ResponseWriter, r *http.Request) {
+
+	// Read the request body using io.ReadAll
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("\n ERROR: Error reading the request body %v", err)
+	}
+
+	var out JVOD
+
+	// JSON unmarshall the request body to UnmarshalledOutput struct
+	_ = json.Unmarshal(body, &out)
+
+	/*
+		if err != nil {
+			log.Printf("\nERROR: Unmarshalling data from request body :: ERROR MESSAGE: %v", err)
+		}
+	*/
+
+	if out.Event == "" {
+		return
+	} else {
+		fmt.Println("=== JVOD State Change Registered ===")
+		fmt.Printf("\nEvent: %s", out.Event)
+		fmt.Printf("\nJob ID: %s", out.JobID)
+		fmt.Printf("\nJob State: %s", out.JvodState)
+		fmt.Printf("\nTime: %v\n", time.Now())
+	}
+}
--- routes/routes.go
+++ routes/routes.go
@@ -15,6 +15,8 @@ func Routes() *http.ServeMux {
 	// Convert our APIListener function to a Handler using mux.HandleFunc which implements the ServeHTTP method
 	mux.HandleFunc("/state-change", handlers.StateChangeListener)
 
+	mux.HandleFunc("/jvod-state-change", handlers.JVODStateChangeListener)
+
 	mux.HandleFunc("/debug/vars", expvar.Handler().ServeHTTP)
 
 	return mux
